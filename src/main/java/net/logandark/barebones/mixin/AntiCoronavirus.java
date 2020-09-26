f1b451ea933189e46fcacccd217eef5c2140bf6a
Logan Darklock <logandarklock@gmail.com>
2020-09-26 13:16:52 -0700

    Barebones v2

--- src/main/java/net/logandark/barebones/mixin/AntiCoronavirus.java
+++ src/main/java/net/logandark/barebones/mixin/AntiCoronavirus.java
@@ -1,4 +1,4 @@
-package net.logandark.boilerplate.mixin;
+package net.logandark.barebones.mixin;
 
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.resource.SplashTextResourceSupplier;
--- src/main/kotlin/net/logandark/barebones/Barebones.kt
+++ src/main/kotlin/net/logandark/barebones/Barebones.kt
@@ -1,11 +1,11 @@
-package net.logandark.boilerplate
+package net.logandark.barebones
 
 import net.fabricmc.api.ModInitializer
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 
 @Suppress("unused")
-object FabricBoilerplate : ModInitializer {
+object Barebones : ModInitializer {
 	val logger: Logger = LogManager.getLogger()
 
 	override fun onInitialize() {
