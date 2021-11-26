3387e9b2713ec82e70b29f84212adda891c55e50
LoganDark <logandarklock@gmail.com>
2021-11-25 17:56:26 -0800

    Update fabric stuff

--- src/main/kotlin/net/logandark/barebones/Barebones.kt
+++ src/main/kotlin/net/logandark/barebones/Barebones.kt
@@ -6,7 +6,7 @@ import org.apache.logging.log4j.Logger
 
 @Suppress("unused")
 object Barebones : ModInitializer {
-	val logger: Logger = LogManager.getLogger()
+	private val logger: Logger = LogManager.getLogger()
 
 	override fun onInitialize() {
 		logger.info("Hello Fabric world!")
